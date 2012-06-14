70d0b5da0cdd8171c8cfb698de2971945ba451c0
Jess Hsu <jesshsu@gmail.com>
2012-06-14 16:33:57 +0000

    RottenPotatoes enhancement #2: filter the list of movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,7 +9,17 @@ class MoviesController < ApplicationController
   def index
     #@movies = Movie.all
     @movies = Movie.find(:all, :order => (params[:sort_by]))
+    if params[:ratings]
+      @movies = Movie.where(:rating => params[:ratings].keys).find(:all, :order => (params[:sort_by]))
+    end
+
     @sort_column = params[:sort_by]
+    @all_ratings = Movie.all_ratings  #setting up @all_ratings variable by controller for Movie model
+    @set_ratings = params[:ratings]
+    if !@set_ratings
+      @set_ratings = Hash.new
+    end
+
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    a = Array.new
+    self.select("rating").uniq.each {|e| a.push(e.rating)}
+    a.sort.uniq
+  end
 end
